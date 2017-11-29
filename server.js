36c33e138d693275c0769f095722b785f30bcce6
jjade seravla <jadealvares@hotmail.co.uk>
2017-11-29 14:27:31 +0000

    need express and mongoDB

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 var http = require('http'),
     fs = require('fs');
-fs.readFile('visit.html', function (err, html) {
+
+fs.readFile('visit.html', function (err, data) {
     if (err) {
         throw err;
     }
+
     http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
+        response.writeHeader(200, {"Content-Type": "text/html", 'Content-Length':data.length});
+        response.write(data);
         response.end();
     }).listen(3000);
+
 });
-
-// var http = require('http');
-// var fs = require("fs");
-//
-// http.createServer(function(request, response) {
-//   fs.readFile("visit.html", function(err, data){
-//   response.writeHead(200, {'Content-Type': 'text/html'});
-//   response.write(data);
-//   response.end();
-// });
-// }).listen(3000);
-
-// var http = require('http');
-//
-//
-// var server = http.createServer(function(req, res) {
-//   res.writeHead(200);
-//   res.end('Visit Http');
-// });
-// server.listen(8080);
