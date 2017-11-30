d86448d053b93819ab6dcc69dfd4a8edbcbf13c4
jjade seravla <jadealvares@hotmail.co.uk>
2017-11-30 12:32:42 +0000

    body parser middleware and set static path

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ var path = require('path');
 
 var app = express();
 
+// var logger = function(req, res, next){
+//   console.log('logging');
+//   next();
+// }
+//
+// app.use(logger);
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Set Static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var person = {
+  name: 'Jeff',
+  age: 30
+}
+
+app.get('/', function(req, res){
+  res.json(person);
+});
+
 app.listen(7000, function(){
   console.log('Server started on port 7000');
 })
